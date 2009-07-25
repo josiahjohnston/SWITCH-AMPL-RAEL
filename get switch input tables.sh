fc285a9f7c7338a44e5759a3e8e5c4dca800671d
Josiah Johnston <siah@berkeley.edu>
2009-07-25 00:38:40 +0000

    Added RPS tables. Also, prints status messages that indicate which files are being pulled from the database.

--- get switch input tables.sh
+++ get switch input tables.sh
@@ -98,45 +98,67 @@ export HOURS_IN_SAMPLE="if( hours_in_sample < 100, 2034, 58986 )"
 # col1_name col2_name ...
 # [rows of data]
 
+echo 'Copying data from the database to input files...'
+
+echo '	study_hours.tab...'
 echo ampl.tab 1 5 > study_hours.tab
 mysql -h $db_server -u $user -p$password -e "select study_hour as hour, period, study_date as date, $HOURS_IN_SAMPLE as hours_in_sample, month_of_year, hour_of_day from wecc.study_hours_all where $TIMESAMPLE order by 1;" >> study_hours.tab
 
+echo '	load_zones.tab...'
 echo ampl.tab 1 2 > load_zones.tab
 mysql -h $db_server -u $user -p$password -e "select replace(load_area, ' ', '_') as load_zone, x_utm as load_zone_x, y_utm as load_zone_y from wecc.load_area;" >> load_zones.tab
 
+echo '	existing_plants.tab...'
 echo ampl.tab 2 12 > existing_plants.tab
 mysql -h $db_server -u $user -p$password -e "select replace(load_area, ' ', '_') as load_zone, plant_code, peak_mw as size_mw, fuel, heat_rate, invsyear as start_year, max_age, overnight_cost, fixed_o_m, variable_o_m, forced_outage_rate, scheduled_outage_rate, baseload, cogen from wecc.existing_plants_agg order by 1, 2;" >> existing_plants.tab
 
+echo '	trans_lines.tab...'
 echo ampl.tab 2 4 > trans_lines.tab
 mysql -h $db_server -u $user -p$password -e "select replace(load_area_start, ' ', '_') as load_zone_start, replace(load_area_end, ' ', '_') as load_zone_end, existing_transmission, tid, length_km as transmission_length_km, transmission_efficiency from wecc.directed_trans_lines;" >> trans_lines.tab
 
 # TODO: adopt better load forecasts; this assumes a simple 1.6%/year increase
+echo '	system_load.tab...'
 echo ampl.tab 2 1 > system_load.tab
 mysql -h $db_server -u $user -p$password -e "select replace(load_area, ' ', '_') as load_zone, study_hour as hour, power(1.016, period-(2004+datediff(datetime_utc, '2004-01-01')/365))*power as system_load from wecc.system_load l join wecc.study_hours_all h on (h.hournum=l.hour) where $TIMESAMPLE order by study_hour, load_zone;" >> system_load.tab
 
+echo '	hydro.tab...'
 echo ampl.tab 3 3 > hydro.tab
 mysql -h $db_server -u $user -p$password -e "select distinct replace(load_area, ' ', '_') as load_zone, site, study_date as date, avg_flow, min_flow, max_flow from wecc.hydro_monthly_limits l join wecc.study_dates_all d on l.year = year(d.date_utc) and l.month=month(d.date_utc) where $DATESAMPLE order by 1, 2, month, year;" >> hydro.tab
 
 echo $SCENARIO > scenario_name.txt
 
+echo '	cap_factor.tab...'
 echo ampl.tab 5 1 > cap_factor.tab
 mysql -h $db_server -u $user -p$password -e "select replace(load_area, ' ', '_') as load_zone, technology, site, orientation, study_hour as hour, cap_factor from wecc.cap_factor_all c join wecc.study_hours_all h on (h.hournum=c.hour) where $SITESAMPLE and $TECHSAMPLE and $TIMESAMPLE;" >> cap_factor.tab
 
+echo '	max_capacity.tab...'
 echo ampl.tab 4 1 > max_capacity.tab
 mysql -h $db_server -u $user -p$password -e "select replace(load_area, ' ', '_') as load_zone, technology, site, orientation, max_capacity*$SCALEUP as max_capacity from wecc.max_capacity_all where $SITESAMPLE and $TECHSAMPLE;" >> max_capacity.tab
 
+echo '	connect_cost.tab...'
 echo ampl.tab 4 2 > connect_cost.tab
 mysql -h $db_server -u $user -p$password -e "select replace(load_area, ' ', '_') as load_zone, technology, site, orientation, connect_length_km, connect_cost_per_MW from wecc.connect_cost_all where $SITESAMPLE and $TECHSAMPLE;" >> connect_cost.tab
 
+echo '	generator_info.tab...'
 echo ampl.tab 1 11 > generator_info.tab
 mysql -h $db_server -u $user -p$password -e "select * from wecc.generator_info;" >> generator_info.tab
 
+echo '	regional_plant_cost_multiplier.tab...'
 echo ampl.tab 1 1 > regional_plant_cost_multiplier.tab
 mysql -h $db_server -u $user -p$password -e "select load_area as load_zone, regional_plant_cost_multiplier from wecc.regional_plant_cost_multiplier_human where scenario_id = $REGIONAL_MULTIPLIER_SCENARIO_ID;" >> regional_plant_cost_multiplier.tab
 
+echo '	regional_fuel_costs.tab...'
 echo ampl.tab 3 1 > regional_fuel_costs.tab
 mysql -h $db_server -u $user -p$password -e "select load_area as load_zone, fuel, year, fuel_cost from wecc.regional_fuel_costs_human where scenario_id = $REGIONAL_FUEL_COST_SCENARIO_ID" >> regional_fuel_costs.tab
 
+echo '	regional_generator_costs.tab...'
 echo ampl.tab 2 8 > regional_generator_costs.tab
 mysql -h $db_server -u $user -p$password -e "select load_area as load_zone, technology, price_year, overnight_cost, connect_cost_per_MW_generic, fixed_o_m, variable_o_m, overnight_cost_change, fixed_o_m_change, variable_o_m_change from wecc.regional_generator_costs_human;" >> regional_generator_costs.tab
 
+echo '	generator_rps.tab...'
+echo ampl.tab 2 1 > generator_rps.tab
+mysql -h $db_server -u $user -p$password -e "select load_area as load_zone, fuel, qualifies from wecc.generator_rps;" >> generator_rps.tab
+
+echo '	rps_requirement.tab...'
+echo ampl.tab 1 1 > rps_requirement.tab
+mysql -h $db_server -u $user -p$password -e "select load_area as load_zone, rps from wecc.rps_requirement;" >> rps_requirement.tab 
