d829f6091783b5f56d260cce140c990950a26503
Josiah Johnston <siah@berkeley.edu>
2010-02-06 04:13:38 +0000

    Change the name of the database from switch_results_wecc_v2 to switch_results_wecc_v2_1 to reflect changes to the schema. Added an import of runtimes (by default, attempts to read from log files. Later I will change it to record this in AMPL. Changed how files are imported to speed things up and allow more control (e.g. ignoring irrelevant columns that make the file more human-readable) Report the time that it takes to import each file. Use appropriate bash scripting syntax to get unique names for temporary files. Removed gen_by_load_area.

--- AMPL/import results to mysql.sh
+++ AMPL/import results to mysql.sh
@@ -4,7 +4,7 @@
 ##########################
 # Constants
 read SCENARIO_ID < scenario_id.txt
-DB_name='switch_results_wecc_v2'
+DB_name='switch_results_wecc_v2_1'
 db_server='switch-db1.erg.berkeley.edu'
 current_dir=`pwd`
 results_dir="results"
@@ -19,14 +19,24 @@ then
 	user=$1
 	password=$2
 else
-	echo "User name for MySQL on $db_server? "
+	echo "User name for MySQL $DB_name on $db_server? "
 	read user
-	echo "Password for MySQL on $db_server? "
+	echo "Password for MySQL $DB_name on $db_server? "
 	stty_orig=`stty -g`   # Save screen settings
 	stty -echo            # To keep the password vaguely secure, don't let it show to the screen
 	read password
 	stty $stty_orig       # Restore screen settings
 fi
+
+###################################################
+# Extract a summary of run times for optimizations
+echo 'Importing run times...'
+runtime_path=runtimes$$.txt
+echo "scenario_id,tax,cost_runtime,trans_runtime" > $runtime_path
+perl -e'open(LOG,"<switch.log"); my $cost,$trans,$tax; while(my $l=<LOG>) { chomp $l; if($l =~ m/^(\d+) seconds to optimize for cost./ ) { $cost=$1;} elsif($cost && $l =~ m/^(\d+) seconds to optimize for transmission./) {$trans = 1} elsif($trans && $l =~m/Tax=(\d+):/) {$tax=$1; print $ARGV[0].",$cost,$trans,$tax\n"; $cost=$trans=0; } } close(LOG);' $SCENARIO_ID >> $runtime_path
+mysql -h $db_server -u $user -p$password -e "use $DB_name; load data local infile \"$runtime_path\" $write_over_prior_results into table run_times fields terminated by \",\" optionally enclosed by '\"' ignore 1 lines;"
+rm $runtime_path
+
 
 ###################################################
 # Clear out the prior instance of this run if requested
@@ -37,7 +47,7 @@ then
 	then
 		rewrite_results="REPLACE";
 		echo "Flushing Prior results for scenario ${SCENARIO_ID}";
-		mysql -h $db_server -u $user -p$password -e "use $DB_name; select clear_scenario_results(${SCENARIO_ID});"
+		mysql -h $db_server -u $user -p$password --column-names=false -e "select clear_scenario_results(${SCENARIO_ID});" $DB_name
 	fi
 fi
 if [ $# -ge 1 ]
@@ -46,39 +56,45 @@ then
 	then
 		rewrite_results="REPLACE";
 		echo "Flushing Prior results for scenario ${SCENARIO_ID}";
-		mysql -h $db_server -u $user -p$password -e "use $DB_name; select clear_scenario_results(${SCENARIO_ID});"
+		mysql -h $db_server -u $user -p$password --column-names=false -e "select clear_scenario_results(${SCENARIO_ID});" $DB_name
 	fi
 fi
 
 ###################################################
-# Import all the results file into the DB
+# Import all of the results files into the DB
 echo 'Importing results files...'
-for file_base_name in gen_cap trans_cap local_td_cap power transmission
+for file_base_name in gen_cap trans_cap local_td_cap dispatch transmission system_load
 do
- for file_name in `ls results/${file_base_name}_*csv | grep "[[:digit:]]"`
+ for file_name in `ls results/${file_base_name}_*txt | grep "[[:digit:]]"`
  do
   file_path="$current_dir/$file_name"
+  echo "    ${file_name}  ->  ${DB_name}._${file_base_name}"
   # Import the file in question into the DB
-  # Almost every output file goes into a table of the same name. The file "power" is the exception, it goes into "dispatch"
-  if [ $file_base_name != power ]
-  then
-    echo "    $file_name  ->  $DB_name.$file_base_name"
-    mysql -h $db_server -u $user -p$password -e "use $DB_name; load data local infile \"$file_path\" $write_over_prior_results into table $file_base_name fields terminated by \",\" optionally enclosed by '\"' ignore 1 lines;"
-  else
-    echo "    $file_name  ->  $DB_name.dispatch"
-    mysql -h $db_server -u $user -p$password -e "use $DB_name; load data local infile \"$file_path\" $write_over_prior_results into table dispatch fields terminated by \",\" optionally enclosed by '\"' ignore 1 lines;"
-  fi
+  case $file_base_name in
+    gen_cap) printf "%20s seconds to import %s rows\n" `(time -p mysql -h $db_server -u $user -p$password -e "load data local infile \"$file_path\" $write_over_prior_results into table _gen_cap (scenario_id, carbon_cost, period, project_id, area_id, @junk, technology_id, @junk, @junk, orientation, new, baseload, cogen, fuel, capacity, fixed_cost);" $DB_name) 2>&1 | grep -e '^real' | sed -e 's/real //'` `wc -l "$file_path" | sed -e 's/^[^0-9]*\([0-9]*\) .*$/\1/g'`
+      ;;
+    trans_cap) printf "%20s seconds to import %s rows\n" `(time -p mysql -h $db_server -u $user -p$password -e "load data local infile \"$file_path\" $write_over_prior_results into table _trans_cap (scenario_id,carbon_cost,period,start_id,end_id,@junk,@junk,tid,new,trans_mw,fixed_cost);" $DB_name) 2>&1 | grep -e '^real' | sed -e 's/real //'` `wc -l "$file_path" | sed -e 's/^[^0-9]*\([0-9]*\) .*$/\1/g'`
+      ;;
+    local_td_cap) printf "%20s seconds to import %s rows\n" `(time -p mysql -h $db_server -u $user -p$password -e "load data local infile \"$file_path\" $write_over_prior_results into table _local_td_cap (scenario_id, carbon_cost, period, area_id, @junk, local_td_mw, fixed_cost);" $DB_name) 2>&1 | grep -e '^real' | sed -e 's/real //'` `wc -l "$file_path" | sed -e 's/^[^0-9]*\([0-9]*\) .*$/\1/g'`
+      ;;
+    dispatch) printf "%20s seconds to import %s rows\n" `(time -p mysql -h $db_server -u $user -p$password -e "load data local infile \"$file_path\" $write_over_prior_results into table _dispatch (scenario_id, carbon_cost, period, project_id, area_id, @junk, study_date, study_hour, technology_id, @junk, @junk, orientation, new, baseload, cogen, fuel, power, co2_tons, hours_in_sample, heat_rate, fuel_cost_tot, carbon_cost_tot, variable_o_m_tot);" $DB_name) 2>&1 | grep -e '^real' | sed -e 's/real //'` `wc -l "$file_path" | sed -e 's/^[^0-9]*\([0-9]*\) .*$/\1/g'`
+      ;;
+    transmission) printf "%20s seconds to import %s rows\n" `(time -p mysql -h $db_server -u $user -p$password -e "load data local infile \"$file_path\" $write_over_prior_results into table _transmission (scenario_id, carbon_cost, period, receive_id, send_id, @junk, @junk, study_date, study_hour, rps_fuel_category, power_sent, power_received, hours_in_sample);" $DB_name) 2>&1 | grep -e '^real' | sed -e 's/real //'` `wc -l "$file_path" | sed -e 's/^[^0-9]*\([0-9]*\) .*$/\1/g'`
+      ;;
+    system_load) printf "%20s seconds to import %s rows\n" `(time -p mysql -h $db_server -u $user -p$password -e "load data local infile \"$file_path\" $write_over_prior_results into table _system_load (scenario_id, carbon_cost, period, area_id, @junk, study_date, study_hour, power, hours_in_sample);" $DB_name) 2>&1 | grep -e '^real' | sed -e 's/real //'` `wc -l "$file_path" | sed -e 's/^[^0-9]*\([0-9]*\) .*$/\1/g'`
+      ;;
+   esac
  done
 done
 
 ###################################################
 # Crunch through the data
 echo 'Crunching the data...'
-echo "use ${DB_name};" > tmp_data_crunch.sql
-echo "set @scenario_id := ${SCENARIO_ID};" >> tmp_data_crunch.sql
-cat CrunchResults.sql >> tmp_data_crunch.sql
-mysql -h $db_server -u $user -p$password < tmp_data_crunch.sql
-rm tmp_data_crunch.sql
+data_crunch_path=tmp_data_crunch$$.sql
+echo "set @scenario_id := ${SCENARIO_ID};" >> $data_crunch_path
+cat CrunchResults.sql >> $data_crunch_path
+mysql -h $db_server -u $user -p$password $DB_name < $data_crunch_path
+rm $data_crunch_path
 
 
 ###################################################
@@ -86,14 +102,15 @@ rm tmp_data_crunch.sql
 
 # Make a temporary file of investment periods
 echo 'Getting a list of Investment periods...'
-mysql -h $db_server -u $user -p$password --column-names=false -e "select distinct(period) from $DB_name.gen_summary order by period;" > tmp_invest_periods.txt
+invest_periods_path=tmp_invest_periods$$.txt
+mysql -h $db_server -u $user -p$password --column-names=false -e "select distinct(period) from gen_summary where scenario_id=$SCENARIO_ID order by period;" $DB_name > $invest_periods_path
 
 # Generation....
 # Build a long query that will make one column for each investment period
 select_gen_summary="SELECT distinct g.scenario_id, g.source, g.carbon_cost"
 while read inv_period; do 
 	select_gen_summary=$select_gen_summary", (select round(avg_power) from $DB_name.gen_summary where source = g.source and period = '$inv_period' and carbon_cost = g.carbon_cost and scenario_id = g.scenario_id) as '$inv_period'"
-done < tmp_invest_periods.txt
+done < $invest_periods_path
 select_gen_summary=$select_gen_summary" FROM $DB_name.gen_summary g;"
 mysql -h $db_server -u $user -p$password -e "CREATE OR REPLACE VIEW $DB_name.gen_summary_by_source AS $select_gen_summary"
 
@@ -103,21 +120,22 @@ mysql -h $db_server -u $user -p$password -e "CREATE OR REPLACE VIEW $DB_name.gen
 select_gen_cap_summary="SELECT distinct g.scenario_id, g.source, g.carbon_cost"
 while read inv_period; do 
 	select_gen_cap_summary=$select_gen_cap_summary", (select round(capacity) from $DB_name.gen_cap_summary where source = g.source and period = '$inv_period' and carbon_cost = g.carbon_cost and scenario_id = g.scenario_id) as '$inv_period'"
-done < tmp_invest_periods.txt
+done < $invest_periods_path
 select_gen_cap_summary=$select_gen_cap_summary" FROM $DB_name.gen_cap_summary g;"
 mysql -h $db_server -u $user -p$password -e "CREATE OR REPLACE VIEW $DB_name.gen_cap_summary_by_source AS $select_gen_cap_summary"
 
 
 # Make a temporary file of generation sources
 echo 'Getting a list of generation technologies...'
-mysql -h $db_server -u $user -p$password --column-names=false -e "select distinct(source) from $DB_name.gen_hourly_summary WHERE scenario_id = $SCENARIO_ID;" > tmp_sources.txt
+sources_path=tmp_sources$$.txt
+mysql -h $db_server -u $user -p$password --column-names=false -e "select distinct(source) from $DB_name.gen_hourly_summary WHERE scenario_id = $SCENARIO_ID;" > $sources_path
 
 # Dispatch summary, 
 # Build a long query that will make one column for each generation source
 select_dispatch_summary="SELECT distinct g.scenario_id, g.carbon_cost, g.study_hour, (select period FROM $DB_name.gen_hourly_summary where scenario_id = g.scenario_id and carbon_cost = g.carbon_cost and study_hour = g.study_hour limit 1) as 'period', (select study_date FROM $DB_name.gen_hourly_summary where scenario_id = g.scenario_id and carbon_cost = g.carbon_cost and study_hour = g.study_hour limit 1) as 'study_date', (select hours_in_sample FROM $DB_name.gen_hourly_summary where scenario_id = g.scenario_id and carbon_cost = g.carbon_cost and study_hour = g.study_hour limit 1) as 'hours_in_sample', (select month FROM $DB_name.gen_hourly_summary where scenario_id = g.scenario_id and carbon_cost = g.carbon_cost and study_hour = g.study_hour limit 1) as 'month', (select month_name FROM $DB_name.gen_hourly_summary where scenario_id = g.scenario_id and carbon_cost = g.carbon_cost and study_hour = g.study_hour limit 1) as 'month_name', (select quarter_of_day FROM $DB_name.gen_hourly_summary where scenario_id = g.scenario_id and carbon_cost = g.carbon_cost and study_hour = g.study_hour limit 1) as 'quarter_of_day', (select hour_of_day FROM $DB_name.gen_hourly_summary where scenario_id = g.scenario_id and carbon_cost = g.carbon_cost and study_hour = g.study_hour limit 1) as 'hour_of_day'"
 while read gen_source; do 
 	select_dispatch_summary="$select_dispatch_summary"", (select round(power) FROM $DB_name.gen_hourly_summary where source='$gen_source' and scenario_id = g.scenario_id and carbon_cost = g.carbon_cost and study_hour = g.study_hour ) as '$gen_source'"
-done < tmp_sources.txt
+done < $sources_path
 select_dispatch_summary="$select_dispatch_summary"" FROM $DB_name.gen_hourly_summary g order by scenario_id, carbon_cost, period, month, hour_of_day;"
 mysql -h $db_server -u $user -p$password -e "CREATE OR REPLACE VIEW $DB_name._gen_hourly_summary_by_source AS $select_dispatch_summary"
 
@@ -126,14 +144,14 @@ mysql -h $db_server -u $user -p$password -e "CREATE OR REPLACE VIEW $DB_name._ge
 select_dispatch_summary="SELECT distinct g.scenario_id, g.carbon_cost, g.study_hour, g.load_area, (select period FROM $DB_name.gen_hourly_summary where scenario_id = g.scenario_id and carbon_cost = g.carbon_cost and study_hour = g.study_hour and load_area = g.load_area limit 1) as 'period', (select study_date FROM $DB_name.gen_hourly_summary where scenario_id = g.scenario_id and carbon_cost = g.carbon_cost and study_hour = g.study_hour and load_area = g.load_area limit 1) as 'study_date', (select hours_in_sample FROM $DB_name.gen_hourly_summary where scenario_id = g.scenario_id and carbon_cost = g.carbon_cost and study_hour = g.study_hour and load_area = g.load_area limit 1) as 'hours_in_sample', (select month FROM $DB_name.gen_hourly_summary where scenario_id = g.scenario_id and carbon_cost = g.carbon_cost and study_hour = g.study_hour and load_area = g.load_area limit 1) as 'month', (select month_name FROM $DB_name.gen_hourly_summary where scenario_id = g.scenario_id and carbon_cost = g.carbon_cost and study_hour = g.study_hour and load_area = g.load_area limit 1) as 'month_name', (select quarter_of_day FROM $DB_name.gen_hourly_summary where scenario_id = g.scenario_id and carbon_cost = g.carbon_cost and study_hour = g.study_hour and load_area = g.load_area limit 1) as 'quarter_of_day', (select hour_of_day FROM $DB_name.gen_hourly_summary where scenario_id = g.scenario_id and carbon_cost = g.carbon_cost and study_hour = g.study_hour and load_area = g.load_area limit 1) as 'hour_of_day'"
 while read gen_source; do 
 	select_dispatch_summary="$select_dispatch_summary"", (select round(power) FROM $DB_name.gen_hourly_summary_la where source='$gen_source' and scenario_id = g.scenario_id and carbon_cost = g.carbon_cost and study_hour = g.study_hour and load_area = g.load_area) as '$gen_source'"
-done < tmp_sources.txt
+done < $sources_path
 select_dispatch_summary="$select_dispatch_summary"" FROM $DB_name.gen_hourly_summary_la g order by scenario_id, carbon_cost, period, month, hour_of_day;"
 mysql -h $db_server -u $user -p$password -e "CREATE OR REPLACE VIEW $DB_name._gen_hourly_summary_la_by_source AS $select_dispatch_summary"
 
 
 # delete the temporary files
-rm tmp_invest_periods.txt
-rm tmp_sources.txt
+rm $invest_periods_path
+rm $sources_path
 
 
 
@@ -151,8 +169,6 @@ echo 'Exporting co2_cc.csv...'
 mysql -h $db_server -u $user -p$password -e "select * from $DB_name.co2_cc WHERE scenario_id = $SCENARIO_ID;" > $results_dir/co2_cc.csv
 echo 'Exporting power_cost_cc.csv...'
 mysql -h $db_server -u $user -p$password -e "select * from $DB_name.power_cost where period=(SELECT max(period) FROM $DB_name.power_cost where scenario_id=$SCENARIO_ID) AND scenario_id = $SCENARIO_ID;" > $results_dir/power_cost_cc.csv
-echo 'Exporting gen_by_load_area.csv...'
-mysql -h $db_server -u $user -p$password --column-names=false -e "select * from $DB_name.gen_by_load_area WHERE scenario_id = $SCENARIO_ID;" > $results_dir/gen_by_load_area.csv
 echo 'Exporting trans_cap_new.csv...'
 mysql -h $db_server -u $user -p$password -e "select * from $DB_name.trans_cap where new and period=(SELECT max(period) FROM $DB_name.trans_cap where scenario_id=$SCENARIO_ID) AND scenario_id = $SCENARIO_ID;" > $results_dir/trans_cap_new.tcsv
 echo 'Exporting trans_cap_exist.csv...'
