8242cb7890025b93316cb254ff27b19aed4d71c1
Josiah Johnston <siah@berkeley.edu>
2010-12-07 00:50:58 +0000

    Changed code to optionally allow AMPL to write the problem file and exit, then have cplex solve the optimization, then call AMPL again to parse cplex's solution. Calling cplex from the command line instead of from AMPL can save significant amounts of memory. To use this funcitonality, invoke AMPL with run_switch.sh
    run_switch.sh now will work on or off of the cluster. Off of the cluster, it assumes that you are starting AMPL in single-thread mode.
    
    There is a new parameter in windsun.run compile_mip_only that defaults to 0. If it is set to 1, then switch.run won't invoke cplex with the solve command, but will instead compile problems, parse any solution files it finds, and write cplex options out to a file.

--- AMPL/run_switch.sh
+++ AMPL/run_switch.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
-if [ $# -ne 1 ]; then
-  echo "This script needs one argument: the number of workers on this job."
-  exit
-fi
 
-WORKER_ID=$(getid | awk '{print $1}')
-NUM_WORKERS=$1
+if [ -z $(which getid) ]; then
+	NUM_WORKERS=1
+	WORKER_ID=0
+else
+	if [ $# -ne 1 ]; then
+		echo "This script needs one argument: the number of workers on this job."
+		exit
+	fi
+	WORKER_ID=$(getid | awk '{print $1}')
+	NUM_WORKERS=$1
+fi
+if [ ! -d logs ]; then mkdir logs; fi
 
 # Make a path for this processes log files based on the name of the node this is running on (hostname) and the process id of this process ($$)
 log_base="logs/"$(hostname)"_pid$$_switch"
 # Start ampl and log appropriately
-echo "About to start AMPL on "$(hostname)" with worker $WORKER_ID of $NUM_WORKERS" > $log_base
-echo "include windsun.run; let worker_id := $WORKER_ID; let num_workers := $NUM_WORKERS; include switch.run; exit;" | ampl 1>$log_base".log" 
-2>$log_base".error_log" 
-echo "AMPL has exited." >> $log_base
+echo "Starting AMPL on "$(hostname)" with worker $WORKER_ID of $NUM_WORKERS. Logs written to $log_base..."
+#printf "AMPL commands are:\n   include windsun.run; let worker_id := $WORKER_ID; let num_workers := $NUM_WORKERS; let compile_mip_only := 1; include switch.run; exit;\n"
+echo "include windsun.run; let worker_id := $WORKER_ID; let num_workers := $NUM_WORKERS; let compile_mip_only := 1; include switch.run; exit;" | ampl 1>$log_base".log"  2>$log_base".error_log" 
+echo "	AMPL finished compiling the problems and has exited. About to solve them with cplex." >> $log_base
+cplex_options=$(sed -e "s/^[^']*'\([^']*\)'.*$/\1/" results/cplex_options)
+for f in $(ls -1 results/*nl); do 
+  base_name=$(echo $f | sed "s/\.nl//"); 
+  if [ ! -f $base_name.sol ]; then 
+#    echo "  cplexamp params are: $base_name -AMPL $cplex_options"
+    cplexamp $base_name -AMPL "$cplex_options" 1>>$log_base"_cplex.log" 2>>$log_base"_cplex.error_log"; 
+    echo "  cplexamp finished solving $base_name"
+  fi; 
+done; 
+echo "	Using AMPL to parse cplex solutions and write result files."
+echo 'include windsun.run; let worker_id := $WORKER_ID; let num_workers := $NUM_WORKERS; let compile_mip_only := 1; include switch.run; exit;' | ampl 1>>$log_base".log"  2>>$log_base".error_log" 
