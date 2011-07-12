4056f6e9a6cb4b11d084d9e99da9f5e0c73fc89d
Josiah Johnston <siah@berkeley.edu>
2011-07-12 20:37:40 +0000

    Script can now start an ssh tunnel for your MySQL connection. Intelligent default for username too.

--- AMPL/get_switch_input_tables.sh
+++ AMPL/get_switch_input_tables.sh
@@ -8,6 +8,7 @@
 # 
 # INPUTS
 #  --help                   Print this message
+#  -t | --tunnel            Initiate an ssh tunnel to connect to the database. This won't work if ssh prompts you for your password.
 #  -u [DB Username]
 #  -p [DB Password]
 #  -D [DB name]
@@ -32,12 +33,15 @@ write_to_path='inputs'
 db_server="switch-db1.erg.berkeley.edu"
 DB_name="switch_inputs_wecc_v2_2"
 port=3306
+ssh_tunnel=0
 
 ###################################################
 # Detect optional command-line arguments
 help=0
 while [ -n "$1" ]; do
 case $1 in
+  -t | --tunnel)
+    ssh_tunnel=1; shift 1 ;;
   -u)
     user=$2; shift 2 ;;
   -p)
@@ -62,21 +66,61 @@ done
 # Get the user name and password 
 # Note that passing the password to mysql via a command line parameter is considered insecure
 #	http://dev.mysql.com/doc/refman/5.0/en/password-security.html
+default_user=$(whoami)
 if [ ! -n "$user" ]
 then 
-	echo "User name for MySQL $DB_name on $db_server? "
+	printf "User name for MySQL $DB_name on $db_server [$default_user]? "
 	read user
+	if [ -z "$user" ]; then 
+	  user="$default_user"
+	fi
 fi
 if [ ! -n "$password" ]
 then 
-	echo "Password for MySQL $DB_name on $db_server? "
+	printf "Password for MySQL $DB_name on $db_server? "
 	stty_orig=`stty -g`   # Save screen settings
 	stty -echo            # To keep the password vaguely secure, don't let it show to the screen
 	read password
 	stty $stty_orig       # Restore screen settings
+	echo " "
 fi
 
-connection_string="-h $db_server --port $port -u $user -p$password $DB_name"
+function clean_up {
+  [ $ssh_tunnel -eq 1 ] && kill -9 $ssh_pid # This ensures that the ssh tunnel will be taken down if the program exits abnormally
+  unset password
+}
+
+function is_port_free {
+  target_port=$1
+  if [ $(netstat -ant | \
+         sed -e '/^tcp/ !d' -e 's/^[^ ]* *[^ ]* *[^ ]* *.*[\.:]\([0-9]*\) .*$/\1/' | \
+         sort -g | uniq | \
+         grep $target_port | wc -l) -eq 0 ]; then
+    return 1
+  else
+    return 0
+  fi
+}
+
+#############
+# Try starting an ssh tunnel if requested
+if [ $ssh_tunnel -eq 1 ]; then 
+  echo "Trying to open an ssh tunnel. If it prompts you for your password, this method won't work."
+  local_port=3307
+  is_port_free $local_port
+  while [ $? -eq 0 ]; do
+    local_port=$((local_port+1))
+    is_port_free $local_port
+  done
+  ssh -N -p 22 -c 3des $db_server -L $local_port/$db_server/$port &
+  ssh_pid=$!
+  sleep 1
+  connection_string="-h 127.0.0.1 --port $local_port -u $user -p$password $DB_name"
+  trap "clean_up;" EXIT INT TERM 
+else
+  connection_string="-h $db_server --port $port -u $user -p$password $DB_name"
+fi
+
 test_connection=`mysql $connection_string --column-names=false -e "select count(*) from existing_plants;"`
 if [ ! -n "$test_connection" ]
 then
