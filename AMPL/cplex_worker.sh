30c3aad2f3fedc755b18f28533f7ce0a680f8763
Josiah Johnston <siah@berkeley.edu>
2016-08-03 14:58:28 -0700

    Rewrote cluster job submission code from Torque/qsub to SLURM. I tested this on the Savio cluster at UC Berkeley and verified that the results did not change. Also simplified some scripts to remove carbon cost looping, a feature that in retrospect made implausible assumptions about an exogenous carbon price remaining flat over the entire study period.

--- AMPL/cplex_worker.sh
+++ AMPL/cplex_worker.sh
@@ -5,12 +5,10 @@ function print_help {
   cat <<END_HELP
 cplex_worker.sh
 SYNOPSIS
-  ./cplex_worker.sh --num_workers W --problems "results/sol0_investment_cost results/sol30_investment_cost results/sol60_investment_cost"
+  ./cplex_worker.sh --problems "results/sol0_investment_cost results/sol30_investment_cost results/sol60_investment_cost"
 INPUTS
 	--problems "results/prob1 results/prob2 ..." 
-	                         Lists the base filename of the optimization problems that need to be divided among workers
-	--num_workers W          Specifies the number of worker processes tasked with the given problems
-	--worker N               Used with is_worker in a non-cluster environment with a fork to indicate the worker id of the child process
+	                         Lists the base filename of the optimization problems
 	--cplex_options "key=value key2=value2 ..."
 END_HELP
 }
@@ -25,16 +23,11 @@ umask 0002
 
 # Default values
 runtime_path='results/run_times.txt'
-num_workers=1
-is_worker=0
+resource_sampling_freq_seconds=30
 
 # Parse the options
 while [ -n "$1" ]; do
 case $1 in
-  -n | --num_workers)
-    num_workers=$2; shift 2 ;;
-	--worker)
-		worker_id=$2; shift 2 ;;
 	--problems)
 		problems=$2; shift 2 ;;
 	--cplex_options)
@@ -46,17 +39,7 @@ case $1 in
 esac
 done
 
-# Determine if this is being run in a cluster environment
-if [ -z $(which getid) ]; then cluster=0; else cluster=1; fi;
-
-# Determine the worker id of this process in a cluster environment if it wasn't passed in as a command-line argument
-if [ -z "$worker_id" ] && [ "$cluster" == 1 ]; then worker_id=$(getid | awk '{print $1}'); fi
-# Hack.. If that didn't work, assume this process is the only worker. 
-if [ -z "$worker_id" ]; then worker_id=0; num_workers=1; fi
-
 # Basic error checking
-if [ -z "$worker_id" ]; then echo "ERROR! worker_id is unspecified."; exit; fi
-if [ -z "$num_workers" ]; then echo "ERROR! num_workers was not specified."; exit; fi
 if [ -z "$problems" ]; then echo "ERROR! problems was not specified."; exit; fi
 
 # Gather some other parameters
@@ -64,27 +47,25 @@ if [ -z "$cplex_options" ]; then cplex_options=$(cat results/cplex_options); fi
 scenario_id=$(grep 'scenario_id' inputs/misc_params.dat | sed 's/[^0-9]//g')
 
 # Solve problems that are associated with this worker id
-prob_number=-1
 printf "problems are '$problems'\n";
 for base_name in $problems; do
-  prob_number=$(($prob_number+1))
   # Skip this problem if there is an available solution.
   if [ -f $base_name".sol" ]; then continue; fi
-  # Skip this problem if it doesn't match up with this worker id
-  if [ $(( $prob_number % $num_workers )) -ne $worker_id ]; then continue; fi
-  # Otherwise, solve this problem
-  carbon_cost=$(echo "$base_name" | sed -e 's_^.*[^0-9]\([0-9][0-9]*\)[^/]*$_\1_')
-  log_base="logs/cplex_optimization_"$carbon_cost"_"$(date +'%m_%d_%H_%M_%S')
+  # Set up logs
+  log_base="logs/cplex_optimization_"$(date +'%m_%d_%H_%M_%S')
+  log="$log_base.log"
+  errlog="$log_base.error_log"
+  profile="$log_base.profile"
   printf "About to run cplex. \n\tLogs are ${log_base}...\n\tcommand is: cplexamp $base_name -AMPL \"$cplex_options\"\n"
-  # Record the date & hostname of the computer this is being run on
-  hostname >$log_base".log"
-  date >>$log_base".log"
-  cat $log_base".log" >$log_base".error_log"
+  hostname >$log
+  date >>$log
+  cp $log $errlog
+  # Start cplex and prepare to monitor its resource usage and runtime
   start_time=$(date +%s);
-  cplexamp $base_name -AMPL "$cplex_options" 1>> $log_base".log" 2>> $log_base".error_log" &
+  cplexamp $base_name -AMPL "$cplex_options" 1>> $log 2>> $errlog &
   cplex_pid=$! ;
   ps_headers="$(ps -o vsize,rssize,%mem,%cpu,time,comm -p $cplex_pid | head -1)"
-  echo "realtime elapsed_wall_time elapsed_cpu_time recent_cpu_usage $ps_headers" > $log_base".profile";
+  echo "realtime elapsed_wall_time elapsed_cpu_time recent_cpu_usage $ps_headers" > $profile;
   ps_output="$(ps -o vsize,rssize,%mem,%cpu,time,comm -p $cplex_pid | tail -1)"
   realtime=$(date +%s);
   starttime=$realtime
@@ -94,8 +75,8 @@ for base_name in $problems; do
   last_cputime=$cputime
   cpuusage=0
   while [ $( is_process_running $cplex_pid ) -eq 1 ]; do
-    printf "%8d %d %d %.2f %s\n" $realtime $elapsedtime $cputime $cpuusage "$ps_output" >> $log_base".profile";
-    sleep 30;
+    printf "%8d %d %d %.2f %s\n" $realtime $elapsedtime $cputime $cpuusage "$ps_output" >> $profile;
+    sleep $resource_sampling_freq_seconds;
     ps_output="$(ps -o vsize,rssize,%mem,%cpu,time,comm -p $cplex_pid | tail -1)"
     last_elapsedtime=$elapsedtime
     last_cputime=$cputime
@@ -108,5 +89,5 @@ for base_name in $problems; do
   runtime=$(($end_time - $start_time))
 
 
-  printf "$scenario_id\t$carbon_cost\tcplex_optimize\t"$(date +'%s')"\t$runtime\n" >> "$runtime_path"
+  printf "$scenario_id\t0\tcplex_optimize\t"$(date +'%s')"\t$runtime\n" >> "$runtime_path"
 done
--- AMPL/run_switch.sh
+++ AMPL/run_switch.sh
@@ -11,7 +11,6 @@ If run on a workstation or server, starts SWITCH using the bash command line.
 This tool attempts to auto-detect the computational environment. If it can find the "getid" tool used on the citris cluster, then it will assume a cluster environment. Otherwise, it will assume a w
 
 INPUTS
-  --num_workers X          The cplex part of the optimization workload will be spread between X simultaneous processes, where each process is working on a different carbon cost scenario. 
   --help                   Print this usage information
   --threads_per_cplex X    Allow CPLEX to use up to X threads for the barrier method. This may increase memory requirements.
 OPTIONAL INPUTS FOR A CLUSTER ENVIRONMENT
@@ -24,47 +23,31 @@ END_HELP
 # Set the umask to give group read & write permissions to all files & directories made by this script.
 umask 0002
 
-# Determine if this is being run in a cluster environment
-if [ -z $(which getid) ]; then cluster=0; else cluster=1; fi;
+# Guess if this is being run in a cluster environment based on availability of
+# a slurm utility for submitting jobs (sbatch).
+if [ -z $(which sbatch) ]; then cluster=0; else cluster=1; fi;
 
 # Determine which queues are available
 if [ $cluster -eq 1 ]; then 
-  cluster_login_name=$(qstat -q | sed -n -e's/^server: //p')
-  case $cluster_login_name in
-    psi) cluster_name="psi" ;;
-    perceus-citris.banatao.berkeley.edu) cluster_name="citris" ;;
-  	*) echo "Unknown cluster. Login node is $cluster_login_name."; exit ;;
-  esac
-  case $cluster_name in
-    psi)
-      queue_6hr=psi
-      queue_24hr=psi
-      queue_72hr=psi
-      queue_168hr=psi
-      ;;
-    citris)
-      queue_6hr=short
-      queue_24hr=normal
-      queue_72hr=normal
-      queue_168hr=normal
-      ;;
-  esac
-fi
+  # Make an arbitrary default of 8 cores for running cplex.
+  num_cores=8
 
-# Determine the number of cores available. Different platform require different strategies.
-if [ $(uname) == "Linux" ]; then 
-	num_cores=$(grep processor /proc/cpuinfo | wc -l | awk '{print $1}')
-elif [ $(uname) == "Darwin" ]; then 
-	num_cores=$(sysctl hw.ncpu | awk '{print $2}')
+# If this isn't running on a cluster, set the default cores for cplex to the
+# number of cpus on this machine.
 else
-	echo "Unknown platform "$(uname); exit; 
+  # Determine the number of cores available. Different platform require different strategies.
+  if [ $(uname) == "Linux" ]; then 
+    num_cores=$(grep processor /proc/cpuinfo | wc -l | awk '{print $1}')
+  elif [ $(uname) == "Darwin" ]; then 
+    num_cores=$(sysctl hw.ncpu | awk '{print $2}')
+  else
+    echo "Unknown platform "$(uname); exit; 
+  fi
 fi
 
 # Default values
 runtime_path='results/run_times.txt'
 opt_runtime=24
-num_workers=1
-is_worker=0
 
 # Parse the options
 while [ -n "$1" ]; do
@@ -75,126 +58,95 @@ case $1 in
     jobname=$2; shift 2 ;;
   -r | --runtime)
     opt_runtime=$2; shift 2 ;;
-	--is_worker)
-		is_worker=1; shift ;;
-	--worker)
-		worker_id=$2; shift 2 ;;
-	--problems)
-		problems=$2; shift 2 ;;
-	--threads_per_cplex)
-		threads_per_cplex=$2; shift 2 ;;
-	-h | --help)
-		print_help; exit ;;
-	*)
+  --problems)
+    problems=$2; shift 2 ;;
+  --threads_per_cplex)
+    threads_per_cplex=$2; shift 2 ;;
+  -h | --help)
+    print_help; exit ;;
+  *)
     echo "Unknown option $1"; print_help; exit ;;
 esac
 done
 
-# Make directories for logs & results if they don't already exist
-mkdir -p logs
-mkdir -p results
+# Ensure logs & results directories exist
+mkdir -p logs results
 
-# Validate the threads_per_cplex input and write it to the load.run file. Default and maximum value are num_cores
+# Validate the threads_per_cplex input and write it to the load.run file.
 if [ -z "$threads_per_cplex" ]; then 
   threads_per_cplex=$num_cores; 
 fi
 sed -i".orig" -e 's/^\(option cplex_options.*\)\(threads=[0-9]*\)\([^0-9].*\)$/\1threads='$threads_per_cplex'\3/' "load.run"
-
+rm load.run.orig
 
 # If a jobname wasn't given, use the directory name this process is running in
 if [ -z "$jobname" ]; then 
-	jobname=$(pwd | sed -e 's|^.*/||'); 
+  jobname=$(pwd | sed -e 's|^.*/||'); 
 fi
 
-# Set up the .qsub files and submit job requests to the queue if this is operating on a cluster
+# If this is a cluster, then set up the .slurm files and submit job requests
 if [ $cluster == 1 ]; then
 
-	qsub_files="compile.qsub   optimize.qsub   export.qsub	transmission_optimization.qsub	present_day_dispatch.qsub"
-	# Process parameter values
-	working_directory=$(pwd)
-	# If the email wasn't specified, try to guess who gets the email notifications
-	if [ -z "$email" ]; then
-		case `whoami` in
-			jnelson) email="jameshenrynelson@gmail.com" ;;
-			siah) email="siah@berkeley.edu" ;;
-			amileva) email="amileva@berkeley.edu" ;;
-			dsanchez) email="dansanch01@gmail.com" ;; 
-		esac
-	fi
+  slurm_files="compile.slurm   optimize.slurm   export.slurm"
+  # For now, don't bother with transmission optimization or present day dispatch cuz they are buggy
+  # transmission_optimization.slurm  present_day_dispatch.slurm
 
-	# Process each qsub file for boilerplate stuff
-	for f in $qsub_files; do
-		# Make sure the qsub templates exists and we can write to them
-		if [ ! -f "$f" ]; then 
-			echo "Expected to find a qsub template file in this directory named $f. Please copy that file to this directory and try again. "; exit; 
-		elif [ ! -w "$f" ]; then 
-			case `uname` in 
-				Linux)  owner=$(stat -c "%U" "$f") ;;
-				Darwin) owner=$(stat -f "%Su" "$f") ;;
-				*) owner="the owner" ;;
-			esac
-			echo "Cannot rewrite the qsub template file $f. Ask $owner to change the permissions ('chmod g+w $f' will probably do). "; exit; 
-		fi
-		action=$(echo $f | sed -e 's/\.qsub//')
+  # If the email wasn't specified, try to look up an email address from /etc/passwd. On the savio cluster, it is part of the comment section:
+  # siah:x:40769:501:Josiah Johnston,siah@berkeley.edu:/global/home/users/siah:/bin/bash
+  if [ -z "$email" ]; then
+    # Use egrep to find the right line, then delete all text up to the comma,
+    # then delete all text after the colon.
+    email=$(egrep "^$USER" /etc/passwd | sed -e 's/^.*,//' -e 's/:.*$//')
+  fi
 
-		# Update these default parameters in each qsub file.
-		[ -n "$jobname" ] && sed -i".orig" -e 's/^#PBS -N .*$/#PBS -N '"$jobname-$action"'/' "$f"
-		[ -n "$email" ] && sed -i".orig" -e 's/^#PBS -M .*$/#PBS -M '"$email"'/' "$f"
-		sed -i".orig" -e 's|^working_dir=.*$|working_dir="'"$working_directory"'"|' "$f"
-		
-		# Update the queue parameter in everything mut the optimize file (we do that one below)
-		if [ "$action" != "optimize" ]; then
-		  runtime=$(sed -n -e 's/^#PBS -l walltime=\([0-9]*\):.*$/\1/p' $f)
-		else
-		  runtime=$opt_runtime
-		  cputime=$(($opt_runtime * $threads_per_cplex))
-      sed -i".orig" -e 's/^#PBS -l walltime=.*$/#PBS -l walltime='$runtime':00:00/' "$f"
-      sed -i".orig" -e 's/^#PBS -l cput=.*$/#PBS -l cput='$cputime':00:00/' "$f"
-		fi
-    # Decide which queue to use
-    if [ $runtime -le 6 ]; then
-      sed -i".orig" -e 's/^#PBS -q .*$/#PBS -q '"$queue_6hr"'/' "$f"
-    elif [ $runtime -le 24 ]; then 
-      sed -i".orig" -e 's/^#PBS -q .*$/#PBS -q '"$queue_24hr"'/' "$f"
-    elif [ $runtime -le 72 ]; then 
-      if [ -n "$queue_72hr" ]; then
-        echo "This cluster ($cluster_name) does not support jobs with this much runtime. Try 24 hours or less."
-        exit;
-      fi
-      sed -i".orig" -e 's/^#PBS -q .*$/#PBS -q '"$queue_72hr"'/' "$f"
-    elif [ $runtime -le 168 ]; then 
-      if [ -n "$queue_168hr" ]; then
-        echo "This cluster ($cluster_name) does not support jobs with this much runtime. Try 72 hours or less."
-        exit;
-      fi
-      sed -i".orig" -e 's/^#PBS -q .*$/#PBS -q '"$queue_168hr"'/' "$f"
-    else
-      echo "This cluster ($cluster_name) does not support jobs with this much runtime."
-      exit;
+  # Process each qsub file for boilerplate stuff
+  for f in $slurm_files; do
+    # Make sure the qsub templates exists and we can write to them
+    if [ ! -f "$f" ]; then 
+      echo "Expected to find a slurm template file in this directory named $f. Please copy that file to this directory and try again. "
+      exit
+    elif [ ! -w "$f" ]; then 
+      case `uname` in 
+        Linux)  owner=$(stat -c "%U" "$f") ;;
+        Darwin) owner=$(stat -f "%Su" "$f") ;;
+        *) owner="the owner" ;;
+      esac
+      echo "Cannot rewrite the slurm template file $f. Ask $owner to change the permissions ('chmod g+w $f' will probably do). "; exit; 
     fi
-	done
+    action=$(echo $f | sed -e 's/\.slurm//')
 
-	# Fill in the OPTIMIZE qsub file
-	f=optimize.qsub
-	# How many nodes & workers per node.
-	sed -i".orig" -e 's/^#PBS -l nodes=.*$/#PBS -l nodes=1:ppn='"$threads_per_cplex"'/' "$f"
+    # Update default parameters in each qsub file.
+    if [ -n "$jobname" ]; then
+      sed -i".orig" -e 's/^#SBATCH --job-name=.*$/#SBATCH --job-name='"$jobname-$action"'/' "$f"
+    fi
+    if [ -n "$email" ]; then
+      sed -i".orig" -e 's/^#SBATCH --mail-user=.*$/##SBATCH --mail-user= '"$email"'/' "$f"
+    fi
+    rm $f.orig
+  done
 
+  # Pass the cplex thread count to the optimize slurm file.
+  f=optimize.slurm
+  sed -i".orig" -e 's/^#SBATCH --cpus-per-task=.*$/#SBATCH --cpus-per-task='"$threads_per_cplex"'/' "$f"
+  rm $f.orig
 
-	
-	echo "Submitting jobs to the scheduler."
-	compile_jobid=$(qsub compile.qsub)
-	optimize_jobid=$(qsub optimize.qsub -W depend=afterok:$compile_jobid)
-	results_jobid=$(qsub export.qsub -W depend=afterok:$optimize_jobid)
-	transopt_jobid=$(qsub transmission_optimization.qsub -W depend=afterok:$optimize_jobid)
-	present_day_dispatch_jobid=$(qsub present_day_dispatch.qsub)
-	echo "Submission successful. Job IDs are:"
-	printf "\t compile.qsub:                    $compile_jobid\n"
-	printf "\t optimize.qsub:                   $optimize_jobid\n"
-	printf "\t export.qsub:                     $results_jobid\n"
-	printf "\t transmission_optimization.qsub:  $transopt_jobid\n"
-	printf "\t present_day_dispatch.qsub:       $present_day_dispatch_jobid\n"
-	
-	exit;
+  echo "Submitting jobs to the scheduler."
+  # sbatch returns a line like so: "Submitted batch job 798920"
+  # Use awk to nab the last column from that line.
+  compile_jobid=$(sbatch compile.slurm | awk '{print $NF}')
+  optimize_jobid=$(sbatch --dependency=afterok:$compile_jobid optimize.slurm | awk '{print $NF}')
+  export_jobid=$(sbatch --dependency=afterok:$optimize_jobid export.slurm | awk '{print $NF}')
+  # Disable these until their bugs are fixed.
+  #transopt_jobid=$(qsub transmission_optimization.qsub -W depend=afterok:$optimize_jobid)
+  #present_day_dispatch_jobid=$(qsub present_day_dispatch.qsub)
+  echo "Submission successful. Job IDs are:"
+  printf "\t compile.slurm:                    $compile_jobid\n"
+  printf "\t optimize.slurm:                   $optimize_jobid\n"
+  printf "\t export.slurm:                     $export_jobid\n"
+  #printf "\t transmission_optimization.qsub:  $transopt_jobid\n"
+  #printf "\t present_day_dispatch.qsub:       $present_day_dispatch_jobid\n"
+  
+  exit;
 fi
 # END OF CLUSTER STUFF
 
@@ -203,26 +155,26 @@ fi
 # If we've made it here, we aren't in a cluster environment. Execute SWITCH appropriately. 
 
 function cpu_usage() {
-	pid=$1
-	tmp_path='logs/tmp.txt'
-	ps S -o ppid,pid,pcpu,pmem > $tmp_path
-	pid_list=$pid
-	op=""
-	while [ "$pid_list" != "$op" ]; do 
-		op="$pid_list"
-		pid_list=$(egrep '^'"$pid_list" $tmp_path | awk 'BEGIN {buf="'$pid'";} {if(buf=="") buf=$2; else buf=sprintf("%s|%d",buf,$2)} END{print buf}')
-	done
-	egrep '^[0-9]* '"$pid_list" $tmp_path | awk '{sum += $3} END{printf "%.2f\n", sum/100}'
-	rm $tmp_path
+  pid=$1
+  tmp_path='logs/tmp.txt'
+  ps S -o ppid,pid,pcpu,pmem > $tmp_path
+  pid_list=$pid
+  op=""
+  while [ "$pid_list" != "$op" ]; do 
+    op="$pid_list"
+    pid_list=$(egrep '^'"$pid_list" $tmp_path | awk 'BEGIN {buf="'$pid'";} {if(buf=="") buf=$2; else buf=sprintf("%s|%d",buf,$2)} END{print buf}')
+  done
+  egrep '^[0-9]* '"$pid_list" $tmp_path | awk '{sum += $3} END{printf "%.2f\n", sum/100}'
+  rm $tmp_path
 }
 
 
-# Compile the problems sequentially with one copy of AMPL
+# Compile with AMPL
 log_base="logs/ampl_compilation_"$(date +'%m_%d_%H_%M_%S')
 printf "Compiling optimization problems. Logging results to $log_base...\n"
 echo 'include load.run; include compile.run;' | ampl 1>$log_base".log"  2>$log_base".error_log" 
 
-# Optimize the problems with N copies of cplex
+# Optimize with CPLEX
 problems=$(                  # Find problems that lack solutions
   find results -name '*nl' | # Search for files in the results directory with an 'nl' suffix. Pipe the results (if any) to the next step. 
   sed -e 's/.nl$//' |        # Strip the '.nl' suffix off the end of the paths. 
@@ -234,33 +186,25 @@ problems=$(                  # Find problems that lack solutions
   done |                     # Pass whatever made it through the filter to the next step
   tr '\n' ' '                # Replace the line returns with spaces so the list of problems is easier to pass around
 ) 
+resource_sampling_freq_seconds=5
 if [ -n "$problems" ]; then  # Only start the optimization step if there are problems to solve
-  printf "Spawning %d workers for cplex optimization...\n" $num_workers
-  log_base="logs/worker_"$(date +'%m_%d_%H_%M_%S')
-  pids=''
-  printf "Worker pids are: "
-  for ((i=0; i<$num_workers; i++)); do
-    ./cplex_worker.sh --num_workers $num_workers --worker $i --problems "$problems" 1>$log_base"-$i.log" 2>$log_base"-$i.log" &
-    pids[$i]=$!
-    printf "%d " ${pids[$i]}
-  done
-  printf "\n";
-  echo "Workers' cpu usage..."
-  still_running=$num_workers
+  printf "Starting cplex optimization...\n"
+  log_base="logs/cplex_"$(date +'%m_%d_%H_%M_%S')
+  ./cplex_worker.sh --problems "$problems" 1>$log_base".log" 2>$log_base".log" &
+  pid=$!
+  printf "process id is %d\n" $pid
+  echo "Cplex cpu usage..."
+  still_running=1
   while [ $still_running -gt 0 ]; do
     still_running=0
-    status_line=""
-    for ((i=0; i<$num_workers; i++)); do
-      if [ $(ps -p ${pids[$i]} | wc -l | awk '{print $1}') -gt 1 ]; then
-        status[$i]=$(cpu_usage ${pids[$i]})
-        still_running=$(($still_running + 1))
-      else
-        status[$i]=done
-      fi
-      status_line="$status_line $i: "${status[$i]}
-    done
-    echo -ne "\r$status_line"
-    sleep 2
+    if [ $(ps -p $pid | wc -l | awk '{print $1}') -gt 1 ]; then
+      statustext=$(cpu_usage $pid)
+      still_running=1
+    else
+      statustext=done
+    fi
+    echo -ne "\r$statustext"
+    sleep $resource_sampling_freq_seconds
   done
   printf "\nOptimization done.\n"
 else 
@@ -268,19 +212,20 @@ else
 fi
 
 
-# Export results with one copy of AMPL
+# Export results with AMPL
 log_base="logs/ampl_export_"$(date +'%m_%d_%H_%M_%S')
 printf "Exporting results from AMPL to text files. Logging results to $log_base...\n"
 echo 'include load.run; include export.run;' | ampl 1>$log_base".log"  2>$log_base".error_log" 
 
-
-# Export results with one copy of AMPL
-log_base="logs/ampl_transopt_"$(date +'%m_%d_%H_%M_%S')
-printf "Optimizing transmission. Logging results to $log_base...\n"
-echo 'include load.run; include transmission_optimization.run;' | ampl 1>$log_base".log"  2>$log_base".error_log" 
-
-
-# Execute present_day_dispatch with one copy of AMPL
-log_base="logs/ampl_present_day_dispatch_"$(date +'%m_%d_%H_%M_%S')
-printf "Starting AMPL present_day_dispatch. Logging results to $log_base...\n"
-echo 'include load.run; include present_day_dispatch.run;' | ampl 1>$log_base".log"  2>$log_base".error_log" 
+# These are buggy, so disable for now.
+if [ 1 -eq 0 ]; then 
+  # Execute transmission optimization with AMPL
+  log_base="logs/ampl_transopt_"$(date +'%m_%d_%H_%M_%S')
+  printf "Optimizing transmission. Logging results to $log_base...\n"
+  echo 'include load.run; include transmission_optimization.run;' | ampl 1>$log_base".log"  2>$log_base".error_log" 
+  
+  # Execute present_day_dispatch with AMPL
+  log_base="logs/ampl_present_day_dispatch_"$(date +'%m_%d_%H_%M_%S')
+  printf "Starting AMPL present_day_dispatch. Logging results to $log_base...\n"
+  echo 'include load.run; include present_day_dispatch.run;' | ampl 1>$log_base".log"  2>$log_base".error_log" 
+fi
